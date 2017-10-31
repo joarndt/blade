986504edbf9212635164b1e5e4da67053e6659b7
joarndt <joarndt94@gmail.com>
2017-10-31 21:43:32 +0100

    added quit and join functionality

--- teamspeakbot/illuminazibot.py
+++ teamspeakbot/illuminazibot.py
@@ -7,31 +7,54 @@ from telepot.loop import MessageLoop
 from message import *
 from client import *
 
+#starts Teamspeak
 def ts_start(auth):
+
+    #some  output for Telegram
+    write_telegram('joining Teamspeak')
+
+    #starts Teamspeak
     cmd = ["ts3"]
     process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-    print 'joining Teamspeak'
     time.sleep(2)
+
+    #initiate Clientquery connection
     client = Client(auth)
     client.subscribe()
     set_ts_running(True)
+    send_whoami(client)
+
     return client
 
+#stops Teamspeak
 def ts_stop(client):
+    
+    #some output for Telegram
+    write_telegram('quitting Teamspeak')
+
+    #close connection and quit Teamspeak
     set_ts_running(False)
     client.close()
     call(["killall", "ts3client_linux_amd64"])
 
-def send_whoami():
+#sends whomai command for verification
+def send_whoami(client):
     com = Command(
         'whoami',
     )
     client.send_command(com)
 
+#sets ts_running variable
 def set_ts_running(tmp):
     global ts_running
     ts_running = tmp
 
+#write message into Telegram chat
+def write_telegram(string):
+    global bot
+    bot.sendMessage(ts3, string)
+
+#Telegram bot loop 
 def handle(msg): 
 
     #get chat id
@@ -47,50 +70,62 @@ def handle(msg):
         if debug: print chat_id
         if debug: print 'Got command: %s' % command
 
-        #writes command for current channelclients
-        if chat_id == ts3 and command=='/status':
-            com = Command(
-                    'channelclientlist cid=' + channelid,
-                )
-            client.send_command(com)
 
-        #unlisten from teamspeakchat
-        elif chat_id == ts3 and command == '/stfu':
-            listen = False
-            bot.sendMessage(ts3,'stopped listening to TS3 Chat')
-
-        #listen to teamspeakchat
-        elif chat_id == ts3 and command == '/listen':
-            listen = True
-            bot.sendMessage(ts3,'started listening to TS3 Chat')
-        
         #quitting teamspeak
-        elif chat_id == ts3 and command == '/quit':
+        if chat_id == ts3 and command == '/quit':
             global client
-            ts_stop(client)
+
+            if ts_running:
+                ts_stop(client)
+            else:
+                write_telegram('Not in Teamspeak')
         
         #joining teamspeak
         elif chat_id == ts3 and command == '/join':
             global client
-            client = ts_start(auth)
-            client.subscribe()
-            send_whoami()
-        
-        #builds textmessages and writes it into teamspeakchat     
-        elif chat_id == ts3:
-            com  = "sendtextmessage targetmode=2 msg="
-            txt = ''
-            if 'username' in msg['from']:
-                txt += msg['from']['username']
-            elif 'first_name' in msg['from']:
-                txt += msg['from']['first_name']
-            txt +=': ' + msg['text']
-            txt = txt.replace(" ","\s")
-            com += txt
-            command = Command(
-                    str(com),
-                )
-            client.send_command(command)
+
+            if ts_running:
+                write_telegram('already in Teamspeak')
+            else:
+                client = ts_start(auth)
+                client.subscribe()
+
+        elif ts_running:
+
+            #writes command for current channelclients
+            if chat_id == ts3 and command=='/status':
+                com = Command(
+                        'channelclientlist cid=' + channelid,
+                    )
+                client.send_command(com)
+
+            #unlisten from teamspeakchat
+            elif chat_id == ts3 and command == '/stfu':
+                listen = False
+                bot.sendMessage(ts3,'stopped listening to TS3 Chat')
+
+            #listen to teamspeakchat
+            elif chat_id == ts3 and command == '/listen':
+                listen = True
+                bot.sendMessage(ts3,'started listening to TS3 Chat')
+            
+            #builds textmessages and writes it into teamspeakchat     
+            elif chat_id == ts3:
+                com  = "sendtextmessage targetmode=2 msg="
+                txt = ''
+                if 'username' in msg['from']:
+                    txt += msg['from']['username']
+                elif 'first_name' in msg['from']:
+                    txt += msg['from']['first_name']
+                txt +=': ' + msg['text']
+                txt = txt.replace(" ","\s")
+                com += txt
+                command = Command(
+                        str(com),
+                    )
+                client.send_command(command)
+        else:
+            write_telegram('bot is not in Teamspeak')
 
 #variable for listening to ts chat 
 listen = True
@@ -100,7 +135,10 @@ ts_running = False
 #variable for invokerid
 invokerid = 0
 channelid = 0
-
+#whoami command
+whoami = Command(
+        'whoami',
+    )
 
 #read necessary data for bot
 file = open('data.txt')
@@ -116,19 +154,14 @@ ts3 = int(file.readline())
 #end of reading
 file.close
 
-#start teamspeak client connection
-client = ts_start(auth)
-
 #start bot with bot_token
 bot = telepot.Bot(token)
 MessageLoop(bot, handle).run_as_thread()
 
-print 'I am listening ...'
+#start teamspeak client connection
+client = ts_start(auth)
 
-whoami = Command(
-        'whoami',
-    )
-client.send_command(whoami)
+print 'I am listening ...'
 
 #listen to teamspeakchat
 while 1:
@@ -141,9 +174,7 @@ while 1:
 
             #outputs teamspeakchat in telegram group
             if message.command == 'notifytextmessage' and message['invokerid']!=invokerid and listen:
-                txt = message['invokername']
-                txt+=':\n'
-                txt+= message['msg']
+                txt = message['invokername'] + ':\n' + message['msg']
                 txt = txt.replace("[URL]","")
                 txt = txt.replace("[/URL]","")
                 bot.sendMessage(ts3,txt)
@@ -157,11 +188,14 @@ while 1:
             elif message.is_response():
                 clients = 'Currently Online:'
                 if debug:  print message
-                if 'client_nickname' in message.keys():
-                    clients += '\n' + message.__getitem__('client_nickname')
-                else :
+
+                if message.is_multipart_message():
                     for part in message.responses:
-                        clients+='\n' + part['client_nickname']
+                        if 'client_nickname' in part.keys():
+                            clients += '\n' + part.__getitem__('client_nickname')
+                else :
+                    if 'client_nickname' in message.keys():
+                        clients += '\n' + message.__getitem__('client_nickname')
                 clients+='\nlisten: '
                 if listen:
                     clients+='True'
