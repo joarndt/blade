e84b857a336497636e589dc37b4fa89f5b98b73b
joarndt <joarndt94@gmail.com>
2017-11-03 10:30:59 +0100

    update notebook files

---
+++ scripts/updateNotebookFiles.sh
@@ -0,0 +1,18 @@
+#update notebookfiles in the git#
+#################################
+
+#copy all costum notebookfiles
+cp -r -b /home/blade/.config/i3/* /home/blade/git/blade/i3wm/notebook_config
+cp -r -b /home/blade/scripts/* /home/blade/git/blade/scripts
+
+#add them to the git
+cd /home/blade/git/blade/i3wm/notebook_config
+git add .
+cd /home/blade/git/blade/scripts
+git add .
+
+#commit  changes
+git commit -m "update notebook files"
+
+#push them into git
+git push
